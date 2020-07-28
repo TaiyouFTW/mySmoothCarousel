61b428eaf0ea49ad887e1ea9d8bfca35459d37e7
Guilherme Batista Moreira <gui.bm.1995@gmail.com>
2020-07-28 14:04:07 -0300

    template images and carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mySmoothCarousel';
+  images: string[];
+  constructor() {
+    this.images = [
+      'https://unsplash.it/1600/400?random=1',
+      'https://unsplash.it/1600/400?random=2',
+      'https://unsplash.it/1600/400?random=3',
+      'https://unsplash.it/1600/400?random=4',
+      'https://unsplash.it/1600/400?random=5',
+      'https://unsplash.it/1600/400?random=6',
+      'https://unsplash.it/1600/400?random=7',
+      'https://unsplash.it/1600/400?random=8',
+      'https://unsplash.it/1600/400?random=9',
+      'https://unsplash.it/1600/400?random=10',
+    ];
+  }
 }
